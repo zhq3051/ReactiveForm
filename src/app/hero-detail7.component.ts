671f6cac9e0d92bd3c6e4224f2e76686308c0497
zeke <zhengq@shinetechchina.com>
2017-03-26 21:20:29 -0700

    list and detail, no add

--- src/app/hero-detail7.component.ts
+++ src/app/hero-detail7.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
-import { Hero, Address } from './data-models';
+import { Hero, Address, states } from './data-models';
 import { HeroService } from './hero.service';
 
 @Component({
@@ -12,6 +12,9 @@ export class HeroDetail7Component implements OnChanges {
 
   @Input() hero: Hero;
   heroForm: FormGroup;
+  nameChangeLOg: string[] = [];
+  states = states;
+
   constructor(private fb: FormBuilder, private heroesService: HeroService) {
     this.createForm();
   }
@@ -22,12 +25,23 @@ export class HeroDetail7Component implements OnChanges {
       secretLairs: this.fb.array([]),
       power: '',
       sidekick: ''
-    })
+    });
   }
 
   ngOnChanges() {
     this.heroForm.reset({
       name: this.hero.name
     });
+    this.setAddress(this.hero.addresses);
+  }
+
+  get secretLairs(): FormArray {
+    return this.heroForm.get('secretLairs') as FormArray;
+  }
+
+  setAddress(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.fb.group(address));
+    const addressFormArray = this.fb.array(addressFGs);
+    this.heroForm.setControl('secretLairs', addressFormArray);
   }
 }
